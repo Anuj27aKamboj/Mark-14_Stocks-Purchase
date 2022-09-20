af1ac13ae8dfa4e3caf8e397e36fd5a0594245ee
Anuj27aKamboj <anujkamboj2000@gmail.com>
2022-09-21 01:51:52 +0530

    update: fixed js

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ function calculateProfitAndLoss(initial, quantity, current){
     if(initial > current){
         var loss = (initial-current) * quantity;
         var lossPercentage = (loss/initial) * 100;
+        lossPercentage = lossPercentage.toFixed(2);
 
         showOutput("Ohh! The loss is ₹"+loss+" and the loss percentage is "+lossPercentage+"%");
-        outputBox.style.color = "red";
     }else if(current > initial){
         var profit = (current-initial) * quantity;
         var profitPercentage = (profit/initial) * 100;
+        profitPercentage = profitPercentage.toFixed(2);
 
         showOutput("Yay The profit is ₹"+profit+" and the profit percentage is "+profitPercentage+"%");
         outputBox.style.color = "green";
@@ -23,6 +24,16 @@ function calculateProfitAndLoss(initial, quantity, current){
 }
 
 function submitHandler(){
+    if(initialPrice.value.length === 0 || stocksQuantity.value.length === 0 || currentPrice.value.length === 0){
+        alert("Input values cannot be empty");
+        return;
+    }
+
+    if(Number(initialPrice.value)<0 || Number(stocksQuantity.value)<0 || Number(currentPrice.value)<0){
+        outputBox.style.display = "block";
+        outputBox.innerText = "Enter valid input";
+        return;
+    } 
     var ip = Number(initialPrice.value);
     var qty = Number(stocksQuantity.value);
     var curr = Number(currentPrice.value);
